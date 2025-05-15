aea312d2b3d827e0d426f223120a45f729c3f665
i.sabirov <i.sabirov@medrc.ru>
2025-05-15 17:03:25 +0300

    Создан: """Тест создания элемента Меню""", """Тест вывода списка элементов Меню""", """Тест обновления элемента Меню""", """Тест удаления элемента Меню""".

---
+++ tests/test_cruds_menu_items.py
@@ -0,0 +1,119 @@
+import pytest
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import List
+
+from src.core.cruds.menu_items import (
+    create_menu_item,
+    get_menu_items_one,
+    get_menu_items_list,
+    update_menu_item,
+    delete_menu_item,
+)
+from src.core.models import MenuItemModel
+from src.core.schemas.menu_items import (
+    MenuItemCreateSchema,
+    MenuItemUpdatePartialSchema,
+)
+
+
+@pytest.mark.asyncio
+async def test_create_menu_item(
+    test_db_session: AsyncSession,
+    clean_db,
+    test_menu_items: List[MenuItemCreateSchema],
+):
+    """Тест создания элемента Меню"""
+
+    menu_item: MenuItemModel = await create_menu_item(
+        session=test_db_session, menu_item_in=test_menu_items[1]
+    )
+
+    # проверка
+    assert menu_item.id == 1
+    assert menu_item.name == "Кофе"
+    assert menu_item.price == 60
+
+
+@pytest.mark.asyncio
+async def test_get_menu_items_list(
+    test_db_session: AsyncSession,
+    clean_db,
+    test_menu_items: List[MenuItemCreateSchema],
+):
+    """Тест вывода списка элементов Меню"""
+
+    for test_menu_item in test_menu_items:
+        await create_menu_item(session=test_db_session, menu_item_in=test_menu_item)
+    menu_item_list = await get_menu_items_list(test_db_session)
+
+    # проверка
+    assert len(menu_item_list) == 9
+
+
+@pytest.mark.asyncio
+async def test_update_menu_item(
+    test_db_session: AsyncSession,
+    clean_db,
+    test_menu_items: List[MenuItemCreateSchema],
+):
+    """Тест обновления элемента Меню"""
+
+    menu_item: MenuItemModel = await create_menu_item(
+        session=test_db_session, menu_item_in=test_menu_items[1]
+    )
+    # проверка текущего состояния
+    assert menu_item.id == 1
+    assert menu_item.name == "Кофе"
+    assert menu_item.price == 60
+
+    order_update_data: MenuItemUpdatePartialSchema = MenuItemUpdatePartialSchema(
+        price=55
+    )
+    menu_item: MenuItemModel = await update_menu_item(
+        session=test_db_session,
+        menu_item=menu_item,
+        item_update=order_update_data,
+        partial=True,
+    )
+
+    # проверка после внесения изменений
+    assert menu_item.id == 1
+    assert menu_item.name == "Кофе"
+    assert menu_item.price == 55
+
+    order_update_data: MenuItemUpdatePartialSchema = MenuItemUpdatePartialSchema(
+        name="Кофе Капучино"
+    )
+    menu_item: MenuItemModel = await update_menu_item(
+        session=test_db_session,
+        menu_item=menu_item,
+        item_update=order_update_data,
+        partial=True,
+    )
+
+    # проверка после внесения изменений
+    assert menu_item.id == 1
+    assert menu_item.name == "Кофе Капучино"
+    assert menu_item.price == 55
+
+
+@pytest.mark.asyncio
+async def test_delete_menu_item(
+    test_db_session: AsyncSession,
+    clean_db,
+    test_menu_items: List[MenuItemCreateSchema],
+):
+    """Тест удаления элемента Меню"""
+
+    for test_menu_item in test_menu_items:
+        await create_menu_item(session=test_db_session, menu_item_in=test_menu_item)
+    menu_item_list = await get_menu_items_list(test_db_session)
+
+    # проверка списка до удаления заказа
+    assert len(menu_item_list) == 9
+
+    await delete_menu_item(session=test_db_session, menu_item=menu_item_list[0])
+    menu_item_list = await get_menu_items_list(test_db_session)
+
+    # проверка списка после удаления заказа
+    assert len(menu_item_list) == 8
