fd907dde6baf2d51a326a2ca2d5557cbe6d99cdb
i.sabirov <i.sabirov@medrc.ru>
2025-05-18 21:01:01 +0300

    Перемещение fastapi_users_router в api/v1

--- src/api/v1/fastapi_users_router.py
+++ src/api/v1/fastapi_users_router.py
@@ -1,7 +1,7 @@
 from fastapi_users import FastAPIUsers
 
-from .backend import authentication_backend
-from .user_manager import get_user_manager
+from src.api.dependencies.authentification.backend import authentication_backend
+from src.api.dependencies.authentification.user_manager import get_user_manager
 from src.core.models import User
 from src.core.types.user_id import UserIdType
 
