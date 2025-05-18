52edca380f130a5e38bdaa9389300ae37fc5a77f
i.sabirov <i.sabirov@medrc.ru>
2025-05-18 21:02:40 +0300

    Регистрация include_router Auth

--- src/api/v1/auth.py
+++ src/api/v1/auth.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
 
+from src.api.v1.fastapi_users_router import fastapi_users
+from src.api.dependencies.authentification.backend import authentication_backend
 from src.core.config import settings
 
 router = APIRouter(
     prefix=settings.api.v1.auth,
     tags=["Auth"],
+)
+
+router.include_router(
+    fastapi_users.get_auth_router(authentication_backend),
 )
