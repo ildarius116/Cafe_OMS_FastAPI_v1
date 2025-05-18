1524e9be6323a46faec3e3c9a88968b7232b4dd0
i.sabirov <i.sabirov@medrc.ru>
2025-05-18 18:11:42 +0300

    Добавлен метод для получения user database

--- src/core/models/users.py
+++ src/core/models/users.py
@@ -1,7 +1,18 @@
+from typing import TYPE_CHECKING
+
 from fastapi_users.db import SQLAlchemyBaseUserTable
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+
 
 from src.core.models import Base
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class UserModel(Base, SQLAlchemyBaseUserTable[int]):
     __tablename__ = "users"
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, UserModel)
