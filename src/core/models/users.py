9d706f312a6fc87563c7d43530c0dc98c5efa824
i.sabirov <i.sabirov@medrc.ru>
2025-05-18 18:51:18 +0300

    Исправлены импорты в User

--- src/core/models/users.py
+++ src/core/models/users.py
@@ -1,8 +1,10 @@
 from typing import TYPE_CHECKING
-from fastapi_users.db import SQLAlchemyBaseUserTable
-from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy import (
+    SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase,
+)
 
-from src.core.models import Base
+from .base import Base
 from src.core.types.user_id import UserIdType
 
 if TYPE_CHECKING:
